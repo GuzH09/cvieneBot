4cc659f5f336c9a2ab203662e5395d34cd6bd52d
GuzH <guzh09@gmail.com>
2025-06-16 23:42:38 -0300

    Test 2

--- src/bot.ts
+++ src/bot.ts
@@ -53,9 +53,7 @@ async function tweetPercentage() {
     const tweet = `Se viene en un ${seVienePercentage.toFixed(2)}%.
 Se viene: ${dataFetch.seViene}
 No se viene: ${dataFetch.noSeViene}
-Total: ${total}
-Fuente: https://cviene.com/`;
-
+Fuente: cviene.com`;
     await client.v2.tweet(tweet);
     console.log('Tweeted:', tweet);
     console.log(`Votes - Se viene: ${dataFetch.seViene}, No se viene: ${dataFetch.noSeViene}, Total: ${total}`);
